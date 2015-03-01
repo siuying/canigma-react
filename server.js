ad7a3c2b06d0bd4b46dbd6f7b6091154f3c1de32
Francis Chong <francis@ignition.hk>
2015-03-02 00:39:13 +0800

    choose hot based on env

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
-var config = (process.env.NODE_ENV == "production") ?  require('./webpack.production.config') : require('./webpack.config')
+var config = (process.env.NODE_ENV == "production") ? require('./webpack.production.config') : require('./webpack.config')
 var port = process.env.PORT ? process.env.PORT : 3000
+var hot = (process.env.NODE_ENV == "production") ? true : false
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
+  hot: hot,
   historyApiFallback: true
 }).listen(port, 'localhost', function (err, result) {
   if (err) {
